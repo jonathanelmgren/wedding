11d7b41ef01ac2a3b02a5ca1791e1367fed2bede
Jonathan Elmgren <jonathan@elmgren.dev>
2024-01-03 18:42:49 +0100

    uxui login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,10 +9,10 @@ const Page = () => {
     }
     return (
         <div className="flex flex-col items-center justify-center h-screen w-screen">
-            Skriv in din token
+            <p className="mb-2">Skriv in koden som du fick på ditt kort</p>
             <form action={login} className="flex">
-                <input name="token" type="text" />
-                <button>OK</button>
+                <input name="token" type="text" className="rounded-r-none" />
+                <button className="border-primary border-l-0 border-[1px] px-4 bg-primary text-white rounded-r-md">OK</button>
             </form>
         </div>
     )

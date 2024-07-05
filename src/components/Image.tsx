7a4183ef9b7023252bfb58da1973565c96e44a9e
Jonathan Elmgren <jonathan@elmgren.dev>
2024-07-05 15:51:06 +0200

    fix last viewed modal

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -5,6 +5,12 @@ import { useState } from "react";
 import { WeddingImage } from "ts/types";
 import { useLastViewed } from "./LastViewedProvider";
 
+const Spinner = () => (
+  <div className="text-primary max-w-16 max-h-16 inline-block h-[40vh] w-[40vw] animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+  </div>
+);
+
 const Image = ({ img, user }: { img: WeddingImage; user: string }) => {
   const [fullSize, setFullSize] = useState(false);
   const [fullSizeLoaded, setFullSizeLoaded] = useState(false);
@@ -28,13 +34,14 @@ const Image = ({ img, user }: { img: WeddingImage; user: string }) => {
         onLoad={() => updateImageLoaded()}
       />
       {fullSize && (
-        <div className="bg-gray-200 h-screen w-screen fixed object-contain inset-0 flex items-center justify-center">
-          {!fullSizeLoaded && <div>Laddar bilden...</div>}
+        <div className="bg-black bg-opacity-40 fixed inset-0 flex items-center justify-center w-full h-full" onClick={() => setFullSize(false)}>
+          {!fullSizeLoaded && (
+              <Spinner />
+          )}
           <img
-            className={fullSizeLoaded ? "block" : "hidden"}
+            className={fullSizeLoaded ? "block max-w-full max-h-full object-contain" : "hidden"}
             onLoad={() => setFullSizeLoaded(true)}
             loading="eager"
-            onClick={() => setFullSize(false)}
             src={img.default.url}
             width={img.default.width}
             height={img.default.height}
--- src/components/ScrollToLastviewedModal.tsx
+++ src/components/ScrollToLastviewedModal.tsx
@@ -39,6 +39,7 @@ const ScrollToLastViewedModal: React.FC<ScrollToLastViewedModalProps> = ({
   }, [onClose]);
 
   const handleYes = () => {
+    setClosed(true);
     scrollToLastViewed();
     onClose();
   };
